43965e1bfaee4debdff48e9c0a9b4ac312dfb3e9
Gustavo Tironi <b51083@fgv.edu.br>
2023-05-05 12:29:36 -0300

    Questão 2 adicionada ao arquivo respostas

--- respostas.py
+++ respostas.py
@@ -11,6 +11,19 @@ def converter_lista_para_string (lista):
 # Questão 2
 
 def pares_e_impares(lista):
+    pares = []
+    impares = []
+    for numero in lista:
+        if numero%2 == 0:
+            pares.append(numero)
+        else:
+            impares.append(numero)
+    pares.sort()
+    impares.sort()
+    resultado = []
+    resultado.append(pares)
+    resultado.append(impares)
+    return resultado
 
 
 # Questão 3
