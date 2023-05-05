66a8352627421c901ba1ef6db4cadb8cc84dc0dd
Gustavo Tironi <b51083@fgv.edu.br>
2023-05-05 12:45:02 -0300

    Questao 3 não finalizada, falta ajustar os valores adicionados

---
+++ questao3.py
@@ -0,0 +1,14 @@
+def cria_matriz(num_linhas, num_colunas):
+    matriz = []
+    valor = num_linhas*num_colunas
+    for i in range(num_linhas):
+        linha = []
+        for j in range(num_colunas):
+            linha.append(numero)
+        matriz.append(linha)
+    return matriz
+
+
+
+print(cria_matriz(2,3))
+
