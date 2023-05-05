07b89fdea6fab79e28f89da5122d21bfc78ca02a
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-05-05 15:04:29 -0300

    Comecei a questão 4

---
+++ questão4.py
@@ -0,0 +1,8 @@
+def filtrar_palavras(frase, letra):
+    palavras = frase.split()
+    for palavra in palavras:
+        
+    count(letra)
+
+
+print(filtrar_palavras("Alo, galera de carro, ai que vai", "a"))
