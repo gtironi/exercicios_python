015044fb6f6abb79eae52c6a4b15af6414251b45
Gustavo Tironi <b51083@fgv.edu.br>
2023-05-05 12:08:51 -0300

    Definido os nomes de cada função

---
+++ respostas.py
@@ -0,0 +1,25 @@
+# Questão 1
+
+def converter_lista_para_string (lista):
+
+
+# Questão 2
+
+def pares_e_impares(lista):
+
+
+# Questão 3
+
+def cria_matriz(linhas, colunas):
+
+
+# Questão 4
+
+def filtrar_palavras(frase, letra):
+
+
+# Questão 5
+
+def eh_perfeito(numero):
+
+
