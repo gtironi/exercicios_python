a2894f73d5b535a306a4229dc9dc946eb1c7aa53
Gustavo Tironi <b51083@fgv.edu.br>
2023-05-06 10:59:43 -0300

    Questão 4 Finalizada

--- questão4.py
+++ questão4.py
@@ -1,8 +1,10 @@
 def filtrar_palavras(frase, letra):
+    lista = []
     palavras = frase.split()
     for palavra in palavras:
-        
-    count(letra)
+        if palavra.count(letra) > 0 or palavra.count(letra.upper()):
+            lista.append(palavra)
+    return lista
 
 
 print(filtrar_palavras("Alo, galera de carro, ai que vai", "a"))
