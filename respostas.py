028e817105b642ca8cf13cbcf9294e404c3e0398
Gustavo Tironi <b51083@fgv.edu.br>
2023-05-06 12:05:43 -0300

    questão 4 alterada adicionada ao arqueivo resposta

--- respostas.py
+++ respostas.py
@@ -47,7 +47,7 @@ def filtrar_palavras(frase, letra):
     lista = []
     palavras = frase.split()
     for palavra in palavras:
-        if palavra.count(letra) > 0 or palavra.count(letra.upper()):
+        if palavra.count(letra.lower()) > 0 or palavra.count(letra.upper()):
             lista.append(palavra)
     return lista
 
