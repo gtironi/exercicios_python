3e162103fad07a2e5b4d7f3bbe827d3daeee6185
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-05-16 11:06:30 +0000

    Problema primeiros n quadrados resolvido

--- ICD/aula_16_05.py
+++ ICD/aula_16_05.py
@@ -1,4 +1,5 @@
 # min e max dicionario
+
 dc = {"Joao": 10, "Carlos": 20, "Maria": 25}
 
 def min_max_dicionario(dict):
@@ -12,5 +13,20 @@ def min_max_dicionario(dict):
             min = numero
     return min, max
 
+# print(min_max_dicionario(dc))
 
+# -------------------------------------------------------------
 
+# n primeiros quadrados
+
+def numeros_quadrados(n):
+    dicionario = dict()
+    for i in range(1, n+1):
+        dicionario[i] = i**2
+    return dicionario
+
+# print(numeros_quadrados(5))
+
+# -------------------------------------------------------------
+
+# 
