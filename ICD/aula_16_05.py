88354cc9829cbe5c24689965eebb7bd4d695bf95
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-05-16 11:29:14 +0000

    Função que remove valores duplicados do dicionario com loop

--- ICD/aula_16_05.py
+++ ICD/aula_16_05.py
@@ -1,6 +1,6 @@
 # min e max dicionario
 
-dc = {"Joao": 10, "Carlos": 20, "Maria": 25}
+dc = {"Joao": 10, "Carlos": 20, "Maria": 25, "Antonio": 20}
 
 def min_max_dicionario(dict):
     valores = list(dict.values())
@@ -29,4 +29,30 @@ def numeros_quadrados(n):
 
 # -------------------------------------------------------------
 
-# 
+# remove os valores duplicados do dicionario com loop
+
+# def remove_duplicados(dicio):
+#     dicio_novo = dict()
+#     lista = []
+#     chaves = list(dicio)
+#     for chave in chaves:
+#         if dicio[chave] not in lista:
+#             dicio_novo[chave] = dicio[chave]
+#         lista.append(dicio[chave])
+#     return dicio_novo
+
+def remove_duplicados(dicio):
+    lista = []
+    chaves = list(dicio)
+    for chave in chaves:
+        if dicio[chave] in lista:
+            del dicio[chave]
+        else:
+            lista.append(dicio[chave])
+    return dicio
+
+print(remove_duplicados(dc))
+
+
+
+        
