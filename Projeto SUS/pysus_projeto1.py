e7b989b3fa7759934414c9e5cfd83f55e6d2604b
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-05-25 17:09:46 +0000

    Fiz por estaado, por mes e por municipio

--- Projeto SUS/pysus_projeto1.py
+++ Projeto SUS/pysus_projeto1.py
@@ -2,19 +2,33 @@ from pysus.online_data import SINAN
 from pysus.preprocessing.decoders import decodifica_idade_SINAN
 import pandas as pd
 
-path = SINAN.download('Leptospirose', 2018)
+# path = SINAN.download('Leptospirose', 2018)
 
-dataframe = pd.read_parquet(path)
-dataframe['DT_NOTIFIC'] = pd.to_datetime(dataframe['DT_NOTIFIC'])
-dataframe.to_csv('Leptospirose.csv', index=False)
-dataframe_lepto_2020 = pd.read_csv('Leptospirose.csv').set_index('DT_NOTIFIC')
+# dataframe = pd.read_parquet(path)
+# dataframe['DT_NOTIFIC'] = pd.to_datetime(dataframe['DT_NOTIFIC'])
+# dataframe.to_csv('Leptospirose.csv', index=False)
+dataframe_lepto_2020 = pd.read_csv('Leptospirose.csv')
+dataframe_lepto_2020['DT_NOTIFIC'] = pd.to_datetime(dataframe_lepto_2020['DT_NOTIFIC'])
+dataframe_lepto_2020.set_index('DT_NOTIFIC', inplace = True)
 print(dataframe_lepto_2020)
 
 # Ano
 
-print(len(dataframe_lepto_2020.index))
+# print(len(dataframe_lepto_2020.index))
 
 # Municipio
 
-agrupado_por_municipio = dataframe_lepto_2020.groupby('ID_MUNICIP').count().sort_values('TP_NOT')
-print(agrupado_por_municipio[['TP_NOT']])
+agrupado_por_municipio = dataframe_lepto_2020.groupby('ID_MUNICIP').count().sort_values('TP_NOT', ascending=False)
+# print(agrupado_por_municipio['TP_NOT'])
+
+# Estado
+
+agrupado_por_estado = dataframe_lepto_2020.groupby('SG_UF').count().sort_values('TP_NOT', ascending=False)
+# print(agrupado_por_estado['TP_NOT'])
+
+# Mês
+
+agrupado_por_mês = dataframe_lepto_2020.resample('M').count()
+print(agrupado_por_mês['TP_NOT'])
+
+# Mês
