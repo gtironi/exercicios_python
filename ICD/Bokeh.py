9b434036c1f25a927ad64c176e7584e200e3e6d1
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-05-25 11:46:15 +0000

    mexendo com dataset iris

--- ICD/Bokeh.py
+++ ICD/Bokeh.py
@@ -1,13 +1,15 @@
 from bokeh.plotting import figure
 from bokeh.io import output_file, save, show
+from sklearn import datasets
+import numpy as np
 
-x = [1,7,13,42]
-y = [2, 14, 26, 84]
+iris = datasets.load_iris()
+x = iris.data[:, 2:3]
+y = iris.data[:, 3:4]
 
-output_file("line_plot.html")
+x_flat = list(np.concatenate(x))
+y_flat = list(np.concatenate(y))
+
+output_file("iris.html")
 
 figure = figure()
-figure.line(x, y)
-save(figure)
-
-
