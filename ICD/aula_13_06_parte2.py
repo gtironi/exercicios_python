a43a0031b7f94c49399a5bcf04cf3a5f143f946f
Gustavo Tironi <126670916+gtironi@users.noreply.github.com>
2023-06-13 12:02:57 +0000

    Annotation exemple

--- ICD/aula_13_06_parte2.py
+++ ICD/aula_13_06_parte2.py
@@ -13,8 +13,14 @@ plot1.circle(x,y,size = 13, color = "OrangeRed", alpha = 0.42)
 plot2 = figure(width = 240, height=240, title = "Triangle Glyphs")
 plot2.triangle(x,y,size = 13, color = "Fuchsia", alpha = 0.42)
 
+span_annotation = Span(location = 13, dimension = "width", line_color = "DeepPink", line_width = 3)
+plot2.add_layout(span_annotation)
+
 plot3 = figure(width = 240, height=240, title = "Square Glyphs")
 plot3.square(x,y,size = 13, color = "SeaGreen", alpha = 0.42)
+
+box_annotation = BoxAnnotation(left = 3, right = 7, fill_color = "DeepPink", fill_alpha = 0.42) 
+plot3.add_layout(box_annotation)
 
 plot = gridplot([[plot1, plot2], [plot3, None]])
 
