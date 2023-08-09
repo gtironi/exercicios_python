1b5ad230987181ac9af40e44118d1722f3eef987
Gustavo Tironi <b51083@fgv.edu.br>
2023-08-09 08:56:33 -0300

    np array de 3 dimensões

--- LP/numpy.py
+++ LP/numpy.py
@@ -17,15 +17,24 @@ import numpy as np
 # Tipo: {a1D.dtype}
 # Tamanho do elemento: {a1D.itemsize}''')
 
-a2D =  np.array([[1.0, 2.0], [3.0, 4.0]])
+# a2D =  np.array([[1.0, 2.0], [3.0, 4.0]])
 
-print(f'''Dados de um a1D:
-Dimensão: {a2D.ndim}
-Tamanho: {a2D.size}
-Tipo: {a2D.dtype}
-Tamanho do elemento: {a2D.itemsize}
-Forma do a2D: {a2D.shape}''')
+# print(f'''Dados de um a2D:
+# Dimensão: {a2D.ndim}
+# Tamanho: {a2D.size}
+# Tipo: {a2D.dtype}
+# Tamanho do elemento: {a2D.itemsize}
+# Forma do a2D: {a2D.shape}''')
 
-print(a2D[0, 0])
+# print(a2D[0, 0])
 
-print(a2D[::-1,::-1])
+# print(a2D[::-1,::-1])
+
+a3D =  np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]], dtype = np.float64)
+
+print(f'''Dados de um a3D:
+Dimensão: {a3D.ndim}
+Tamanho: {a3D.size}
+Tipo: {a3D.dtype}
+Tamanho do elemento: {a3D.itemsize}
+Forma do a2D: {a3D.shape}''')
