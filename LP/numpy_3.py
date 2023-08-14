bdb7469c890c0ab2e836bd3915fd7199a192b1cc
Gustavo Tironi <B51083@fgv.edu.br>
2023-08-14 08:05:01 -0300

    Operacoes com array

---
+++ LP/numpy_3.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+import numpy.random as npr
+
+lista_a = [1,2,3]
+lista_b = [4,5,6]
+
+a1D_a = np.array([1,2,3,4])
+a1D_b = np.array([0,1,2,3])
+
+print('Multiplicação:', a1D_a * a1D_b, '\n')
+
+print('Multiplicação por inteiro:', a1D_a * 3, '\n')
+
+print('Soma:',a1D_a + 3, '\n')
+
+print('Subtração:', a1D_a - 3, '\n')
+
+print('Exponencial:', a1D_a ** 3, '\n')
+
+print('Divisão:', a1D_a / 3, '\n')
+# print('Divisão por 0:', a1D_a / 0, '\n')
