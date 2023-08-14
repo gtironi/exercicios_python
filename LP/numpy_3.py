8b28bf35879402a42d200c1703cf35235cddedab
Gustavo Tironi <B51083@fgv.edu.br>
2023-08-14 08:20:13 -0300

    Filtros com True e False

--- LP/numpy_3.py
+++ LP/numpy_3.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import numpy.random as npr
 
+'''
 lista_a = [1,2,3]
 lista_b = [4,5,6]
 
@@ -20,3 +21,60 @@ print('Exponencial:', a1D_a ** 3, '\n')
 
 print('Divisão:', a1D_a / 3, '\n')
 # print('Divisão por 0:', a1D_a / 0, '\n')
+
+a1D_a = a1D_a.reshape(1,4)
+a1D_b = a1D_b.reshape(4,1)
+
+print(a1D_a)
+print(a1D_b, '\n')
+
+print(a1D_a * a1D_b, '\n')
+
+ndarray = np.array([1,2,3,4,5,6])
+indices = ndarray > 3
+
+print(indices)
+print(ndarray[indices], '\n')
+
+indices = (ndarray%2 == 0)
+print(ndarray[indices], '\n')
+'''
+
+ndarray = np.arange(9).reshape(3,3)
+indices = np.array([[False, True, False], [True, False, True], [True, True, True]])
+print(ndarray, '\n')
+print(indices, '\n')
+
+print(ndarray[indices], '\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
