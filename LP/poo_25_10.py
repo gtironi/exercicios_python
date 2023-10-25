bc6b6a950bf2c48e46d2b52843af40ad7d9b89a8
Gustavo Tironi <b51083@fgv.edu.br>
2023-10-25 09:04:19 -0300

    Tudo errado

--- LP/poo_25_10.py
+++ LP/poo_25_10.py
@@ -1,10 +1,16 @@
-class UndergraduateStudent(self):
+class UndergraduateStudent:
 
     def __init__(self, name, student_id, year, major):
         self.name = name
         self.student_id = student_id
         self.year = year
         self.major = major
+        UndergraduateStudent.total_students += 1
+    
+    @staticmethod #não precisa ter objeto para chamar
+    def total_students_count(): #sem self
+        return UndergraduateStudent.total_students
+
 
 class MathStudent(UndergraduateStudent):
     
@@ -16,3 +22,4 @@ class MathStudent(UndergraduateStudent):
     def solve_math_problem(self, problem):
         return f"{self.name} is solving a {self.specialization} math problem: {problem}"
 
+print(f'Total Students: {UndergraduateStudent.total_students_count()}')
